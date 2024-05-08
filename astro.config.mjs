c7c13c3b884b892a65eb17c7baf945615bad316d
Tomek Marciniak <tmarciniakm@gmail.com>
2024-05-08 09:59:39 +0200

    chore(app): add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,32 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import expressiveCode from "astro-expressive-code";
-
 import starlight from "@astrojs/starlight";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   output: "server",
-  integrations: [
-    expressiveCode(),
-    mdx(),
-    sitemap(),
-    tailwind(),
-    svelte(),
-    starlight({
-      title: "Pallad Docs",
-      titleDelimiter: "-",
-      sidebar: [
-        { label: "Introduction to Pallad", link: "/docs" },
-        { label: "Key Management", link: "/docs/key-management" },
-        { label: "Providers", link: "/docs/providers" },
-        { label: "Vault", link: "/docs/vault" },
-      ],
-    }),
-  ],
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), svelte(), starlight({
+    title: "Pallad Docs",
+    titleDelimiter: "-",
+    sidebar: [{
+      label: "Introduction to Pallad",
+      link: "/docs"
+    }, {
+      label: "Key Management",
+      link: "/docs/key-management"
+    }, {
+      label: "Providers",
+      link: "/docs/providers"
+    }, {
+      label: "Vault",
+      link: "/docs/vault"
+    }]
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
