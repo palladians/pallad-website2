9ea9debfcc9e7c1a18b3ea7c93ded8e5e8010368
Tomek Marciniak <tmarciniakm@gmail.com>
2024-06-04 16:26:16 +0200

    feat(website): add analytics

---
+++ src/components/analytics.svelte
@@ -0,0 +1,10 @@
+<script lang="typescript">
+import mixpanel from "mixpanel-browser"
+import { onMount } from "svelte"
+
+onMount(() => {
+  mixpanel.init("ee1f7980362645f72b80bfdd0c7be4a8", {
+    track_pageview: true,
+  })
+})
+</script>
