dd179f6da745e657861d36cf649a156e15c41d2a
Tomek Marciniak <tmarciniakm@gmail.com>
2024-06-12 17:03:15 +0200

    chore(api): update proxy

--- src/pages/api/obscura/[network].json.ts
+++ src/pages/api/obscura/[network].json.ts
@@ -1,10 +1,7 @@
 import type { APIRoute } from "astro";
-import { ofetch } from "ofetch";
 
-export const POST: APIRoute = ({ params, request }) => {
+export const POST: APIRoute = async ({ params, request }) => {
   const proxiedUrl = `https://mina-${params.network}.obscura.network/v1/${import.meta.env.OBSCURA_API_KEY}/graphql`;
-  return ofetch(proxiedUrl, {
-    method: "POST",
-    body: request.body,
-  });
+  const response = await fetch(proxiedUrl, request);
+  return new Response(response.body);
 };
