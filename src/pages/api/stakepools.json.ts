ffea61351035d705ca577a03545f138334e6c260
Tomek Marciniak <tmarciniakm@gmail.com>
2024-06-28 08:44:46 +0200

    feat(stakepools): adjust url

--- src/pages/api/stakepools.json.ts
+++ src/pages/api/stakepools.json.ts
@@ -1,7 +1,7 @@
 import { ofetch } from "ofetch"
 
 const STAKE_POOLS_URL =
-  "https://minascan.io/mainnet/api/api/validators/new-rewards?page=0&limit=100&sortBy=amountStaked&orderBy=DESC&size=100&stake=1000&epoch=77&isFullyUnlocked=true&type=active"
+  "https://minascan.io/mainnet/api/api/validators/?page=0&limit=100&sortBy=amount_staked&orderBy=DESC&size=100&stake=1000&epoch=1&isFullyUnlocked=true&type=active&isNotAnonymous=false&isWithFee=false&isVerifOnly=false"
 
 export async function GET() {
   const data = await ofetch(STAKE_POOLS_URL)
